ec4955b5c1050da2aa3a45023eba79ca51816eb8
WilmerBuestan <wobuestan@espe.edu.ec>
2026-02-03 12:52:27 -0500

    Fix: Usar backticks correctos en JS

--- public/app.js
+++ public/app.js
@@ -1,14 +1,8 @@
-function suma(a,b){
+function suma(a, b) {
     return a + b;
 }
 
-document.getElementById('out').textContent = `6 + 9 = ${suma(6,9)}`;
-
+// IMPORTANTE: Fíjate que las comillas de abajo son INCLINADAS (`), no rectas (')
+document.getElementById('out').textContent = `6 + 9 = ${suma(6, 9)}`;
 
 console.log('Hola mundo desde app.js');
-
-
-
-
-
-
